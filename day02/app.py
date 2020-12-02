94b3596cad1e5aa5d228f23317b63fce7ba7b10b
Gustavo Nobrega <gustavofn@gmail.com>
2020-12-02 10:21:24 -0800

    Added day02

---
+++ day02/app.py
@@ -0,0 +1,26 @@
+okay=0
+failed=0
+
+f=open("input.txt","r")
+lines=f.readlines()
+result = []
+for x in lines:
+    x = x.strip() # remove end of line
+    x = x.replace(':', '') # remove :
+    x = x.replace('-', ' ') # replace - by space
+    x = x.split(' ') # split
+    count = x[3].count(x[2])
+    #print(x[3], x[2], 'occurences:', count)
+    if int(x[0]) <= count <= int(x[1]):
+        okay += 1
+    else:
+        failed += 1
+
+print('Okay: ', okay)
+print('Failed: ', failed)
+
+f.close()
+
+#for i in result:
+#    for j in i:
+#        print(j, end = ' ')
