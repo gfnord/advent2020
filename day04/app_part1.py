f20fa8294948ba82d74afc4c1d4ee4947b2078a2
Gustavo Nobrega <gustavofn@gmail.com>
2020-12-04 09:48:10 -0800

    Added part 1 day04

---
+++ day04/app_part1.py
@@ -0,0 +1,21 @@
+import re
+
+def checkpassport():
+    valid = 0
+    required_fields = ['byr:', 'iyr:', 'eyr:', 'hgt:', 'hcl:', 'ecl:', 'pid:']
+    input = open('input.txt', 'r').read()
+    blank_line_regex = r"(?:\r?\n){2,}"
+    all_pass = []
+    all_pass = re.split(blank_line_regex, input.strip())
+    for passport in all_pass:
+        passport = passport.replace('\n', ' ')
+        passport = passport.split(' ')
+        count_fields = 0
+        for field in required_fields:
+            if any(field in s for s in passport):
+                count_fields += 1
+        if count_fields == 7:
+            valid += 1
+    print('Valid passports:', valid)
+
+checkpassport()
