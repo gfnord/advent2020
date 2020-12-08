221e5f61d7c65266c9df7ac4319112c6c5f1bb70
Gustavo Nobrega <gustavofn@gmail.com>
2020-12-08 10:39:29 -0800

    Added part1 day08

---
+++ day08/app_part1.py
@@ -0,0 +1,22 @@
+# Read the input
+lines = [x.strip() for x in open('input.txt', 'r').readlines()]
+index = 0
+ran_lines = []
+accumulator = 0
+condition = True
+
+while index < len(lines) and index not in ran_lines:
+    command_line = lines[index].split(' ')
+    #print(index, command_line[0],  command_line[1], ran_lines, condition)
+    if command_line[0] == 'nop': 
+        ran_lines.append(index) 
+        index += 1
+    if command_line[0] == 'acc':
+        accumulator += int(command_line[1])
+        ran_lines.append(index) 
+        index += 1
+    if command_line[0] == 'jmp':
+        ran_lines.append(index) 
+        index += int(command_line[1])
+
+print('Accumulator value:', accumulator)
